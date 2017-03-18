e37ccbe8aff3a38e3efbc39fa129edac79504d0e
Piotr Marecki <pmarecki.pro@gmail.com>
2017-03-18 12:36:56 +0100

    Add comment for clarity

--- src/main/java/nowy/A.java
+++ src/main/java/nowy/A.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 public class A {
     public static void main(String[] args) {
+        //Tworzenie nowego hashseta przy pomocy funkcji google/guava:
         Set<Integer> zbior = Sets.newHashSet(1, 4, 6, 8);
         System.out.println(zbior);
     }
