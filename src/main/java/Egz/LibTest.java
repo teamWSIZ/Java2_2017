2d5cd6ee09c3853bab429d1e9376c561aec1a22f
Piotr Marecki <pmarecki.pro@gmail.com>
2017-05-13 10:55:29 +0200

    egzamin

---
+++ src/main/java/Egz/LibTest.java
@@ -0,0 +1,22 @@
+package Egz;
+
+import com.google.common.collect.Sets;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+class DDD {
+    int x;
+}
+
+public class LibTest {
+
+    public static void main(String[] args) {
+        Set<Integer> ss = Sets.newHashSet();  //wykorzystanie Guava
+
+        DDD ddd = new DDD();    //Wykorzytanie Lombok
+        ddd.setX(12);
+        System.out.println(ddd);
+    }
+}
